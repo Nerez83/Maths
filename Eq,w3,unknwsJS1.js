78d954b7af75b7ce5de82be96b3cc48d11825d7d
Nerez83 <91799513+Nerez83@users.noreply.github.com>
2021-10-13 14:25:23 +0200

    Update Eq,w3,unknwsJS1.js

--- Eq,w3,unknwsJS1.js
+++ Eq,w3,unknwsJS1.js
@@ -1,5 +1,7 @@
 function Equals() {
 
+    const Start = Date.now()
+
     let ExpX1 = parseInt(document.getElementById("x1").value)
     let ExpY1 = parseInt(document.getElementById("y1").value)
     let ExpZ1 = parseInt(document.getElementById("z1").value)
@@ -39,64 +41,61 @@ function Equals() {
 
     let p2 = 0
     let p3 = 0
+    ERRline.textContent = "."
+
+    if(isNaN(ExpX1)){
+        ERRline.textContent += " x1"
+    }
+    if(isNaN(ExpY1)){
+        ERRline.textContent += " y1"
+    }
+    if(isNaN(ExpZ1)){
+        ERRline.textContent += " z1"
+    }
+    if(isNaN(Exp1)){
+        ERRline.textContent += " e1"
+    }
+    if(isNaN(ExpX2)){
+        ERRline.textContent += " X2"
+    }
+    if(isNaN(ExpY2)){
+        ERRline.textContent += " y2"
+    }
+    if(isNaN(ExpZ2)){
+        ERRline.textContent += " z2"
+    }
+    if(isNaN(Exp2)){
+        ERRline.textContent += " e1"
+    }
+    if(isNaN(ExpX3)){
+        ERRline.textContent += " X3"
+    }
+    if(isNaN(ExpY3)){
+        ERRline.textContent += " y3"
+    }
+    if(isNaN(ExpZ3)){
+        ERRline.textContent += " z3"
+    }
+    if(isNaN(Exp3)){
+        ERRline.textContent += " e3"
+    }
+
 
     if (isNaN (ExpX1) || isNaN (ExpY1) || isNaN (ExpZ1) || isNaN (Exp1)  || isNaN (ExpX2)  || isNaN (ExpY2) || isNaN (ExpZ2) || isNaN (Exp2) || isNaN(ExpX3) || isNaN (ExpY3) || isNaN (ExpZ3) || isNaN (Exp3) ) {
         console.error ("NaN")
-        ERRline.textContent = "Something is NaN."
-    }
-    
-    switch (isNaN) {
-        case ExpX1:
-            ERRline.textContent = "X1 is Nann"
-            break
-        case ExpY1:
-            ERRline.textContent = "Y1 is Nann"
-            break
-        case ExpZ1:
-            ERRline.textContent = "Z1 is Nann"
-            break
-        case Exp1:
-            ERRline.textContent = "1 is Nann"
-            break
-        case ExpX2:
-            ERRline.textContent = "X2 is Nann"
-            break
-        case ExpY2:
-            ERRline.textContent = "Y2 is Nann"
-            break
-        case ExpZ2:
-            ERRline.textContent = "Z2 is Nann"
-            break
-        case Exp2:
-            ERRline.textContent = "2 is Nann"
-            break
-        case ExpX3:
-            ERRline.textContent = "X3 is Nann"
-            break
-        case ExpY3:
-            ERRline.textContent = "Y3 is Nann"
-            break
-        case ExpZ3:
-            ERRline.textContent = "Z3 is Nann"
-            break
-        case Exp3:
-            ERRline.textContent = "3 is Nann"
-            break
-        default:
-            console.log("Ignore this")
-
+        ERRline.textContent += " is NaN."
     }
 
     y1 = -ExpY1 / ExpX1
     z1 = -ExpZ1 / ExpX1
     e1 = -Exp1 / ExpX1
-    let line1 = "x = (" + Math.round(y1*10000)/10000 + ")y + (" + Math.round(z1*10000)/10000 + ")z + (" + Math.round(e1*10000)/10000 + ")"
+    const line1 = "x = (" + Math.round(y1*10000)/10000 + ")y + (" + Math.round(z1*10000)/10000 + ")z + (" + Math.round(e1*10000)/10000 + ")"
     EqLine1.textContent = line1
 
     p2 = ExpX2 * y1 + ExpY2
     z2 = (ExpX2 * z1 + ExpZ2) / -p2
     e2 = (ExpX2 * e1 + Exp2) / -p2
-    let line2 = "y = (" + Math.round(z2*10000)/10000 + ")z + (" + Math.round(e2*10000)/10000 + ")"
+    const line2 = "y = (" + Math.round(z2*10000)/10000 + ")z + (" + Math.round(e2*10000)/10000 + ")"
     EqLine2.textContent = line2
 
     p3 = (y1 * e2 + e1) * ExpX3 + ExpY3 * e2 + Exp3
@@ -105,19 +104,23 @@ function Equals() {
     z = -p3 / z3
     y = z2 * z + e2
     x = y1 * y + z1 * z + e1
-    let linex = "x = " + Math.round(x*10000)/10000
-    let liney = "y = " + Math.round(y*10000)/10000
-    let linez = "z = " + Math.round(z*10000)/10000
+    const linex = "x = " + Math.round(x*10000)/10000
+    const liney = "y = " + Math.round(y*10000)/10000
+    const linez = "z = " + Math.round(z*10000)/10000
 
-    if (ExpX1 * x + ExpY1 * y + ExpZ1 * z + Exp1 !== 0) {
+    const result1 = ExpX1 * x + ExpY1 * y + ExpZ1 * z + Exp1
+    const result2 = ExpX2 * x + ExpY2 * y + ExpZ2 * z + Exp2
+    const result3 = ExpX3 * x + ExpY3 * y + ExpZ3 * z + Exp3
+
+    if (result1 > 0.00001 && result1 < -0.00001) {
         console.error ("Error 1 ")
         
     }
-    if (ExpX2 * x + ExpY2 * y + ExpZ2 * z + Exp2 !== 0) {
+    if (result2 > 0.00001 && result2 < -0.00001) {
         console.error ("Error 2 ")
         
     }
-    if (ExpX3 * x + ExpY3 * y + ExpZ3 * z + Exp3 !== 0) {
+    if (result3 > 0.00001 && result3 < -0.00001) {
         console.error ("Error 3 ")
         
     }
@@ -126,4 +129,5 @@ function Equals() {
     EqLineY.textContent = liney
     EqLineZ.textContent = linez
 
+    console.log(`This operation took approximately ${Date.now() - Start} ms.`)
 }
